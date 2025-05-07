7ee709ff369dce2355ff3e6c8bbc9bc56fe78b1c
jaddis53 <jayaddisu@gmail.com>
2025-05-07 20:23:32 +0300

    Leetcode 3174

---
+++ index.py
@@ -0,0 +1,26 @@
+class Solution:
+    def clearDigits(self, s:str)-> str:
+        res=[]
+        delete_cnt = 0
+        def isdigit(c):
+           return ord("0") <= ord(c) <= ord("9")
+
+        for i in reversed(range(len(s))):
+            if isdigit(s[i]):
+                delete_cnt += 1
+            elif delete_cnt:
+                delete_cnt-=1
+            else:
+                res.append(s[i])
+        
+        return "".join(res[::-1])
+    
+
+solution=Solution()
+
+input_string="abc3d4"
+
+result=solution.clearDigits(input_string)
+print(result)
+            
+
