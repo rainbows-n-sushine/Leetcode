5e1706ebf70969bd5615c6160b4e275cf72296bb
rainbows-n-sunshine <jalaladdisu@gmail.com>
2025-05-07 22:10:41 +0300

    Again with logic in statement

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
+# first take a string input(Assuming that thestring is written in a such a way that characters come first before digits and the preceeding digits are always less or equal to the number of characters after)
+#loop through the reversed string
+# check if the current character is a digit, if so skip appending character and increase delete_count by adding 1 to it.
+# check if it is a character
+# if it is a character, check if delete_count is 0, if it is then append the character to empty list res
+# if it is a character,and delete_count was greater than zero, update delete_count subtracting 1 and skip appending to res-list
+
 class Solution:
     def clearDigits(self, s:str)->str:
         res=[]
         delete_count=0
 
         def isDigit(c):
-            return ord("0") <= ord(c) <=ord("9")
-        
+            return  ord("0")<=ord(c)<=ord("9")
+
+
         for i in reversed(range(len(s))):
             if s[i].isdigit():
                 delete_count+=1
@@ -13,12 +21,9 @@ class Solution:
                 delete_count-=1
             else:
                 res.append(s[i])
-
         return "".join(res[::-1])
-
+            
 solution=Solution()
-input_string="abdy453"
-result=solution.clearDigits(input_string)
+input_str="abdhf675"
+result=solution.clearDigits(input_str)
 print(result)
-
-          
